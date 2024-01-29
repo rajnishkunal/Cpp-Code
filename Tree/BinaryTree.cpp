3bf05f077b848538631f82449258b98ece175378
Kunal Rajnish <rajnishkunal0621@gmail.com>
2024-01-29 23:13:01 +0530

    BinaryTree Building

---
+++ Tree/BinaryTree.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int element){
+        data=element;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* createTree()
+{
+    cout<<"Enter the value of data"<<endl;
+    int data;
+    cin>>data;
+    if(data==-1)
+        return NULL;
+
+    Node *newNode=new Node(data);
+    cout<<"Enter left data for child "<<data<<endl;
+    newNode ->left= createTree();
+    cout<<"Enter right data for child "<<data<<endl;
+    newNode ->right= createTree();
+
+    return newNode;
+}
+int main()
+{
+    Node* root=NULL;
+    root=createTree();
+    return 0;
+}
